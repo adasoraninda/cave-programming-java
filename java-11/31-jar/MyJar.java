5bb6c5b091cc4d920b24ef1e5855d2d00a6260ff
adasoraninda <adasoraninda@gmail.com>
2021-06-07 12:35:07 +0700

    Add section-01 (Hello World), section-32 (Jar files), section-34 (Modules)

---
+++ java-11/31-jar/MyJar.java
@@ -0,0 +1,7 @@
+public class MyJar {
+    public static void main(String[] args) {
+        System.out.println("Hello Jar Files");
+    }
+    // to run jar files: 
+    // java -r java-file.jar
+}
---
+++ java-11/34-modules/data/MyModule.java
@@ -0,0 +1,7 @@
+package data;
+
+public class MyModule {
+    public static void main(String[] args) {
+        System.out.println("Hello modules!");
+    }
+}
